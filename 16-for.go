c621872fb0bf9780a1a1673f7d54b40e7efd82dd
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2021-05-14 17:22:03 -0400

    variations on a for loop

--- 16-for.go
+++ 16-for.go
@@ -3,9 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	sum := 0
-	for i := 0; i < 10; i++ {
-		sum += i
+	sum := 1
+	/*
+		for init; condition; post {
+			enclosed scope
+		}
+		for i := 0; i < 10; i++ {
+			sum += i
+		}
+	*/
+	/*
+		init & post are optional
+		becomes 'while' loop
+	*/
+	/*
+		for sum < 1000 {
+			sum += sum
+		}
+	*/
+	/*
+		init, condition, post are optional
+		becomes infite loop
+	for {
+		fmt.Println("inside loop")
 	}
 	fmt.Println(sum)
 }
