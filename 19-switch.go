865c4576c0dc5a1b2575e48d7050e689db2f4cb2
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2021-05-15 13:01:15 -0400

    .

---
+++ 19-switch.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("linux.")
+	default:
+		fmt.Printf("%s.\n", os)
+	}
+	// no fallthrough in go
+	// switch & cases can be include expressions
+}
