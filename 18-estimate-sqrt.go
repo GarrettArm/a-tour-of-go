0882c00bcd1d278eda489c96441b34366668fd82
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2021-05-15 11:57:26 -0400

    for loop condition when close to right answer

--- 18-estimate-sqrt.go
+++ 18-estimate-sqrt.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func Sqrt(x float64) float64 {
 	z := 1.0
-	for i := 0; i < 10; i++ {
+	for i := 0; math.Abs(z*z-x) > 0.000005; i++ {
+		fmt.Printf("Step #%v:\t%v\t%v\n", i, z, math.Abs(z*z-x))
 		z -= (z*z - x) / (2 * x)
 	}
 	return z
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(2.0))
 }
