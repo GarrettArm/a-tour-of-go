04bbc00d769f1c5dd0f40861e9d3bf52780cb973
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2021-05-15 13:03:11 -0400

    exploring defer

--- 22-defer.go
+++ 22-defer.go
@@ -5,5 +5,10 @@ import "fmt"
 func main() {
 	defer fmt.Println("world")
 
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
 	fmt.Println("hello")
+	// defers unwrap in last-in first-out
 }
