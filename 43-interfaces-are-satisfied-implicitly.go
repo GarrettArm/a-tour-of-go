198716f4724e5d5640238092ca65c7fe21bca284
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2021-05-16 18:48:37 -0400

    gathered all the previous examples

--- 43-interfaces-are-satisfied-implicitly.go
+++ 43-interfaces-are-satisfied-implicitly.go
@@ -2,10 +2,37 @@ package main
 
 import (
 	"fmt"
+	// "math"
 )
 
 type I interface {
 	M()
+}
+
+type T struct {
+	S string
+}
+
+func (t T) M() {
+	fmt.Println(t.S)
+}
+
+type R struct {
+	S string
+}
+
+func (r *R) M() {
+	if r == nil {
+		fmt.Println("<nil>")
+		return
+	}
+	fmt.Println(r.S)
+}
+
+type F float64
+
+func (f F) M() {
+	fmt.Println(f)
 }
 
 func describe(i I) {
@@ -14,8 +41,22 @@ func describe(i I) {
 
 func main() {
 	var i I
+
+	// i = &T{"Hello"}
+	// describe(i) // (&{Hello}, *main.T)
+	// i.M()       // Hello
+
+	// i = F(math.Pi)
+	// describe(i) // (3.1415, main.F)
+	// i.M()       // 3.1415
+
+	// var r *R
+	// i = r
+	// describe(i) // (<nil>, *main.R)
+	// i.M()       // <nil>
+
 	describe(i) // (<nil>, <nil>)
-	i.M()       // panic
+	i.M()       // run-time error
 	// nil interface has no value or concrete type
 	// i.M() cannot know which type M() belongs to.
 }
