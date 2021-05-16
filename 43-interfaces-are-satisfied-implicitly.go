f0619bf55ec5cca07d8a14f3f0dc124fb586b833
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2021-05-16 18:36:49 -0400

    nil interface cannot take a method call

--- 43-interfaces-are-satisfied-implicitly.go
+++ 43-interfaces-are-satisfied-implicitly.go
@@ -8,33 +8,14 @@ type I interface {
 	M()
 }
 
-type T struct {
-	S string
-}
-
-func (t *T) M() {
-	if t == nil {
-		fmt.Println("<nil>")
-		return
-	}
-	fmt.Println(t.S)
-}
-
 func describe(i I) {
 	fmt.Printf("(%v, %T)\n", i, i)
 }
 
 func main() {
 	var i I
-	var t *T
-	i = t
-	describe(i) // (<nil>, *main.T)
-	i.M()       // <nil>
-	// empty structure is nil
-	// but interface with empty value is non-nil
-	// nil-receiver.M() can be handled gracefully.
-
-	i = &T{"hello"}
-	describe(i) // (&{hello}, *main.T)
-	i.M()       // hello
+	describe(i) // (<nil>, <nil>)
+	i.M()       // panic
+	// nil interface has no value or concrete type
+	// i.M() cannot know which type M() belongs to.
 }
