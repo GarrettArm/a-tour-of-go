fa7651a24d83e1084664cc6564245b0fc49b7d74
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2021-05-16 14:02:36 -0400

    Abs as a method on Vertex

--- 40-methods.go
+++ 40-methods.go
@@ -9,12 +9,12 @@ type Vertex struct {
 	X, Y float64
 }
 
-func Abs(v Vertex) float64 {
+func (v Vertex) Abs() float64 {
 	// Abs() as a regular function
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func main() {
 	v := Vertex{3, 4}
-	fmt.Println(Abs(v))
+	fmt.Println(v.Abs())
 }
