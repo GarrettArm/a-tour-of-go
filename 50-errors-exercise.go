6eafb7a5cd46262e3e19b1489ce57fe753c78f99
Garrett Armstrong <garrett.w.armstrong@gmail.com>
2021-05-17 19:44:46 -0400

    creating a new error

--- 50-errors-exercise.go
+++ 50-errors-exercise.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func Sqrt(x float) (float64, error) {
-	return 0, nil
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+	return math.Sqrt(x), nil
 }
 
 func main() {
